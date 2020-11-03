dbf98c5d90c92e9cb656dad98b5dc030cfbab41d
Fadi Z. Hania <fhania@ccast.edu>
2020-11-03 11:42:37 +0200

    first commit

--- index.php
+++ index.php
@@ -7,6 +7,5 @@
 </head>
 <body>
     <header<h1>Git VC</h1>></header>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt tempora illo quae possimus reiciendis sed saepe commodi hic tempore aut quia, explicabo enim rerum architecto expedita, maiores accusamus iure error nam quaerat, provident fugit similique! Fugit, ad pariatur? Ab officia eligendi totam architecto corrupti nisi consequuntur? Labore voluptates ipsa nam sunt itaque quidem incidunt impedit ratione odit eum dolores molestias vel, natus doloribus? Dolor magni, reprehenderit laborum explicabo laudantium deleniti ipsum amet facere hic unde fuga veniam tenetur nihil nostrum voluptate vero et, beatae id earum! Inventore, quod iste iusto sit quam fuga, accusamus eligendi repellat doloribus beatae mollitia quasi.</p>
 </body>
 </html>
